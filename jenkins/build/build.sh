23ebadb7eddb95bb2976bdce674c3598c1f6824b
phuongvtn1992 <58924342+phuongvtn1992@users.noreply.github.com>
2021-10-12 12:43:46 +0700

    Update build.sh

--- jenkins/build/build.sh
+++ jenkins/build/build.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
-#cp -f pipeline6/java-app/target/*.jar pipeline6/jenkins/build/
-
 echo "****************************"
 echo "** Building Docker Image ***"
 echo "****************************"
 echo "Value: $@"
+check_img_exist(){
+  if [[ "$(docker images -q $1 2> /dev/null)" != "" ]]; then
+    docker image rm $1
+  fi
+}
+  
 if [ $@ = "NodeJS" ]
 then
-   cd $WORKSPACE/node-hello && docker image rm nodejsapp:latest && docker build -t nodejsapp:latest .
+   cd $WORKSPACE/node-hello && check_img_exist nodejsapp:latest && docker build -t nodejsapp:latest .
 elif [ $@ = "Python" ]
 then
-   cd $WORKSPACE/b9-python-app && docker image rm pythonapp:latest && docker build -t pythonapp:latest .
+   cd $WORKSPACE/b9-python-app && check_img_exist pythonapp:latest && docker build -t pythonapp:latest .
 elif [ $@ = "All" ]
 then
-   cd $WORKSPACE/node-hello && docker image rm nodejsapp:latest && docker build -t nodejsapp:latest .
-   cd $WORKSPACE/b9-python-app && docker image rm pythonapp:latest && docker build -t pythonapp:latest .
+   cd $WORKSPACE/node-hello && check_img_exist nodejsapp:latest && docker build -t nodejsapp:latest .
+   cd $WORKSPACE/b9-python-app && check_img_exist pythonapp:latest && docker build -t pythonapp:latest .
 else
    echo "Not buid anything"
 fi	
